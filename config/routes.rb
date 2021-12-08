529d9a57c83e62023f0955e7a12f39b77a96bd30
Leonardo Cabral <leonardocabral@pop-os.localdomain>
2021-12-08 17:32:50 -0300

    Development of Products Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace 'api' do 
+    namespace 'v1' do
+      scope 'products' do
+        get 'index', to:'products#index'
+        get 'show/:id', to:'products#show'
+        get 'category/:type_id', to: 'products#show_category'
+        post 'create', to:'products#create'
+        patch 'update/:id', to:'products#update'
+        delete 'delete/:id', to:'products#delete'
+      end
+    end
+  end
+
+
 end
